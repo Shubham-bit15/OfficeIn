50efbb37df0d3558751d44bec2936ac9ee821ffb
Shubham-bit15 <shubhamp1504@gmail.com>
2026-05-24 11:53:02 +0530

    Delete OfficeIn-Demo/config.js

--- OfficeIn-Demo/config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-      apiKey: "AIzaSyCyLsqH3ApE9i7N27u63go3Gs8alssXoIA",
-      authDomain: "officein-demo.firebaseapp.com",
-      projectId: "officein-demo",
-      storageBucket: "officein-demo.firebasestorage.app",
-      messagingSenderId: "806376685825",
-      appId: "1:806376685825:web:b783c933f0e546d1e45b00"
-    }
